6cb5fdec2b90ab0e6db7a16a33ca7d4ec983ea21
olgatorres123 <tg.olga@gmail.com>
2020-12-10 12:41:00 -0500

    endponit buscar room y crear

--- Proyecto_Hotel/apis/search_rooms_api.py
+++ Proyecto_Hotel/apis/search_rooms_api.py
@@ -10,4 +10,13 @@ async def get_room_by_state(id_room : int):
         return {"menssage":database_room[id_room]}
     raise HTTPException(status_code = 404, detail = "No hay habitaciones disponibles")
 
+
+@api.get("/administracion/cuartos/{room_number}")
+async def get_room(room_number: str):    
+    RoomInDB = get_room(room_number)
+    if RoomInDB == None:
+     raise HTTPException(status_code=404,detail="The room does not exist")
+    room_out = RoomOut(**room_in_db.dict())               
+    return room_out
+
   
